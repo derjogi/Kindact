b7a51939923d91daa9f9d73833f662989f1e0960
Jonas <derjogi@gmail.com>
2026-05-25 17:04:20 +1200

    Port Jules code to kindact-hc

--- holochain/kindact-hc/dnas/global_registry/zomes/coordinator/registry/src/lib.rs
+++ holochain/kindact-hc/dnas/global_registry/zomes/coordinator/registry/src/lib.rs
@@ -1,29 +1,70 @@
 use hdk::prelude::*;
 use registry_integrity::*;
 
-// Called the first time a zome call is made to the cell containing this zome
 #[hdk_extern]
 pub fn init() -> ExternResult<InitCallbackResult> {
     Ok(InitCallbackResult::Pass)
 }
 
-// Don't modify this enum if you want the scaffolding tool to generate appropriate signals for your entries and links
+#[hdk_extern]
+pub fn register_cell(cell: CellEntry) -> ExternResult<ActionHash> {
+    create_entry(EntryTypes::Cell(cell))
+}
+
+#[hdk_extern]
+pub fn publish_anchor_link(link: AnchorLinkEntry) -> ExternResult<ActionHash> {
+    let anchor_entry = AnchorEntry {
+        name: link.anchor_name.clone(),
+    };
+    let anchor_hash = hash_entry(EntryTypes::Anchor(anchor_entry.clone()))?;
+
+    if get(anchor_hash.clone(), GetOptions::default())?.is_none() {
+        create_entry(EntryTypes::Anchor(anchor_entry))?;
+    }
+
+    let action_hash = create_entry(EntryTypes::AnchorLink(link.clone()))?;
+    create_link(
+        anchor_hash,
+        link.issue_id,
+        LinkTypes::AnchorToIssue,
+        (),
+    )?;
+
+    Ok(action_hash)
+}
+
+#[hdk_extern]
+pub fn get_issues_for_anchor(anchor_name: String) -> ExternResult<Vec<ActionHash>> {
+    let anchor_hash = hash_entry(EntryTypes::Anchor(AnchorEntry { name: anchor_name }))?;
+    let links = get_links(
+        LinkQuery::try_new(anchor_hash, LinkTypes::AnchorToIssue)?,
+        GetStrategy::Network,
+    )?;
+
+    let issue_hashes = links
+        .into_iter()
+        .filter_map(|link| ActionHash::try_from(link.target).ok())
+        .collect();
+
+    Ok(issue_hashes)
+}
+
+#[hdk_extern]
+pub fn create_jurisdictional_claim(
+    claim: JurisdictionalClaimEntry,
+) -> ExternResult<ActionHash> {
+    create_entry(EntryTypes::JurisdictionalClaim(claim))
+}
+
+#[hdk_extern]
+pub fn get_jurisdictional_claims(_: ()) -> ExternResult<Vec<JurisdictionalClaimEntry>> {
+    // Stub: in production we'd query by scope. For the prototype, return empty.
+    Ok(vec![])
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum Signal {}
 
-// Whenever an action is committed, we emit a signal to the UI elements to reactively update them
 #[hdk_extern(infallible)]
-pub fn post_commit(committed_actions: Vec<SignedActionHashed>) {
-    // Don't modify the for loop if you want the scaffolding tool to generate appropriate signals for your entries and links
-    for action in committed_actions {
-        if let Err(err) = signal_action(action) {
-            error!("Error signaling new action: {:?}", err);
-        }
-    }
-}
-
-// Don't modify this function if you want the scaffolding tool to generate appropriate signals for your entries and links
-fn signal_action(action: SignedActionHashed) -> ExternResult<()> {
-    Ok(())
-}
+pub fn post_commit(_committed_actions: Vec<SignedActionHashed>) {}
--- holochain/kindact-hc/dnas/manhattan_windturbine/zomes/coordinator/wind_turbine/src/lib.rs
+++ holochain/kindact-hc/dnas/manhattan_windturbine/zomes/coordinator/wind_turbine/src/lib.rs
@@ -1,28 +1,55 @@
 use hdk::prelude::*;
+use wind_turbine_integrity::*;
 
-// Called the first time a zome call is made to the cell containing this zome
 #[hdk_extern]
 pub fn init() -> ExternResult<InitCallbackResult> {
     Ok(InitCallbackResult::Pass)
 }
 
-// Don't modify this enum if you want the scaffolding tool to generate appropriate signals for your entries and links
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DiscoveryResult {
+    pub anchor_name: String,
+    pub issue_hashes: Vec<ActionHash>,
+}
+
+#[hdk_extern]
+pub fn create_issue(issue: IssueEntry) -> ExternResult<ActionHash> {
+    create_entry(EntryTypes::Issue(issue))
+}
+
+#[hdk_extern]
+pub fn post_comment(comment: CommentEntry) -> ExternResult<ActionHash> {
+    create_entry(EntryTypes::Comment(comment))
+}
+
+#[hdk_extern]
+pub fn discover_issues(anchor_name: String) -> ExternResult<DiscoveryResult> {
+    // Stub: a real implementation would `call` into the registry cell.
+    Ok(DiscoveryResult {
+        anchor_name,
+        issue_hashes: vec![],
+    })
+}
+
+#[hdk_extern]
+pub fn grant_guest_access(_issue_hash: ActionHash) -> ExternResult<CapSecret> {
+    let secret = generate_cap_secret()?;
+
+    let mut functions: HashSet<GrantedFunction> = HashSet::new();
+    functions.insert((zome_info()?.name, "post_comment".into()));
+
+    create_cap_grant(CapGrantEntry {
+        tag: "guest-access".into(),
+        access: CapAccess::Transferable { secret },
+        functions: GrantedFunctions::Listed(functions),
+    })?;
+
+    Ok(secret)
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum Signal {}
 
-// Whenever an action is committed, we emit a signal to the UI elements to reactively update them
 #[hdk_extern(infallible)]
-pub fn post_commit(committed_actions: Vec<SignedActionHashed>) {
-    // Don't modify the for loop if you want the scaffolding tool to generate appropriate signals for your entries and links
-    for action in committed_actions {
-        if let Err(err) = signal_action(action) {
-            error!("Error signaling new action: {:?}", err);
-        }
-    }
-}
-
-// Don't modify this function if you want the scaffolding tool to generate appropriate signals for your entries and links
-fn signal_action(action: SignedActionHashed) -> ExternResult<()> {
-    Ok(())
-}
+pub fn post_commit(_committed_actions: Vec<SignedActionHashed>) {}
